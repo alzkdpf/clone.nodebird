155436313546b99fd55b34696ceaa78e8b20ee40
alzkdpf <code21032@gmail.com>
2021-05-03 01:05:18 +0900

    section 2-5

--- work/src/components/LoginForm.js
+++ work/src/components/LoginForm.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import { Form } from 'antd'
+import React, { useState, useCallback } from 'react'
+import { Form, Input, Button } from 'antd'
+import Link from 'next/link'
 
 const LoginForm = () => {
+    const [id, setId] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onChangeId = useCallback(() => {
+        setId(e.target.value)
+    }, [])
+    
+    const onChangePassword = useCallback(() => {
+        setPassword(e.target.value)
+    },[])
+
     return (
         <>
             <Form>
                 <div>
                     <label htmlFor="user-id">Id</label>
                     <br />
-                    <Input name="user-id"/>
+                    <Input name="user-id" value={id} onChangedId={onChangeId} required />
                 </div>
                 <div>
-                    
+                    <label htmlFor="user-password">password</label>
+                    <br />
+                    <Input name="user-password" value={password} onChangedId={onChangePassword} required/>
                 </div>
-                <div>
-                    
+                <div style={{marginTop:'10px'}}>
+                    <button type="primary" htmlType="submit" loading={false}>로그인</button>
+                    <Link href="/signup"><a><Button>회원가입</Button></a></Link>
                 </div>
                 <div>
                     
